69fccab76f8edae56ae66aae8357e3157a1400d1
Jessica <liqiuyang@linkfin-tech.com>
2020-01-10 11:33:23 +0800

    fifth update

---
+++ WXOfficialAccount/js/baseUrl.js
@@ -0,0 +1,32 @@
+//sit test
+// function getUrl() {
+//     var baseUrl = "http://zlhjsit.vipgz1.idcfengye.com";
+//     return "";
+// }
+//
+// function redictUrl() {
+//     var redictUrl = "http://zlhjsit.vipgz1.idcfengye.com";
+//     return redictUrl;
+// }
+
+//uat
+function getUrl() {
+    var baseUrl = "http://zlhjuat.vipgz1.idcfengye.com";
+    return "";
+}
+
+function redictUrl() {
+    var redictUrl = "http://zlhjuat.vipgz1.idcfengye.com";
+    return redictUrl;
+}
+
+//prod
+// function getUrl() {
+//     var baseUrl = "http://www.huijiefinance.net";
+//     return "";
+// }
+//
+// function redictUrl() {
+//     var redictUrl = "http://www.huijiefinance.net";
+//     return redictUrl;
+// }
